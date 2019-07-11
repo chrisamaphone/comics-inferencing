0f983ff746fe58ee651ad0bf307a1b334e488cfc
R. E. Cardona-Rivera <rogelio@cs.utah.edu>
2019-07-11 17:07:51 -0600

    bug in htn expand

--- typescript/src/htn.ts
+++ typescript/src/htn.ts
@@ -10,40 +10,39 @@ interface PrimitiveAction {
     effects : string[]
 }
 
-interface Decomp {
-    name : string,
-    subplan : action[]
-}
-
 // Composite action:
 //  - Action type
 //  - Label
 //  - List of decomposition methods
 interface CompositeAction {
-    action_type : string,
-    decompositions: Decomp[]
+    action_type : string
 }
 
 type action = PrimitiveAction | CompositeAction
 
+interface Decomp {
+    name : string,
+    subplan : action[]
+}
+
 type plan = PrimitiveAction[]
 
-/* HTN is either
+/* Solution is either
     - Node:
         - action_type
         - label
-        - decomp
-        - list of children (HTNs)
+        - list of children (Solutions)
  - Leaf
     - primitive action
 */
-interface HTN_Node {
+interface SolutionNode {
     action_type : string,
-    decomp : Decomp,
-    children : HTN[]
+    children : Solution[]
 }
 
-type HTN = HTN_Node | PrimitiveAction
+type Solution = SolutionNode | PrimitiveAction
+
+// Test domain: propel by explosion
 
 const primitive_blast : PrimitiveAction =
     {
@@ -80,23 +79,97 @@ const fall_repeat : Decomp =
     }
 
 const composite_falling : CompositeAction = {
-    action_type : "falling",
-    decompositions : [fall_repeat]
+    action_type : "falling"
 }
 
-const falling_node : HTN = {
-    action_type : "falling",
-    decomp : fall_repeat,
-    children : [primitive_fall, primitive_fall_prime, primitive_fall_prime]
+const decomp_propel : Decomp =
+{
+    name: "blast_fall_land",
+    subplan: [primitive_blast, composite_falling, primitive_land]
 }
 
+export const composite_propel_by_explosion : CompositeAction = {
+    action_type : "propel_by_explosion"
+}
 
-// Not right yet
-const example_HTN : HTN =
-    {
-        action_type : "propel by explosion",
-        decomp : fall_repeat,
-        children : []
+// decomps maps composite node names to instances of the Decomp type.
+// The values matching an existing composite node must always be nonempty lists.
+function decomps(name : string) : Decomp[] {
+    if (name == "falling")
+        return [fall_repeat];
+    if (name == "propel_by_explosion")
+        return [decomp_propel];
+    return [];
+}
+
+const falling_node : Solution = {
+    action_type : "falling",
+    children : []
+}
+
+/* expandHTN(htn) recursively decomposes composite nodes into 
+    primitive actions, creating a Solution (tree whose leaves 
+    are primitive actions) along the way.
+
+  Requires htn to be an action list of length at least 1.
+
+  Test1:
+  expandHTN(composite_propel_by_explosion) =
+        {
+            action_type: "propel_by_explosion",
+            children: [
+                primitive_blast,
+                {
+                    action_type: "falling",
+                    children: [
+                        primitive_fall,
+                        primitive_fall',
+                        primitive_fall'
+                    ]
+                },
+                primitive_land
+            ]
+        }
+*/
+
+function randomInterval(min : number, max : number) : number {
+    return Math.floor(Math.random()*(max+1)) + min;
+}
+
+// a.length must be > 0
+export function randomMember(a:any[]) : any {
+    return a[randomInterval(0,a.length-1)];
+}
+
+export function expandHTN(action : action) : Solution {
+    // If the action, is primitive, return it (leaf of the solution tree)
+    if((action as PrimitiveAction).preconditions) {
+        return (action as PrimitiveAction);
     }
+
+    // Otherwise, if it is composite, recursively expand by looking in the
+    // dictionary (decomps) for a decomposition of the composite.
+    const comp = action as CompositeAction;
+    const candidate_subplans = decomps(comp.action_type);
+    if(candidate_subplans) {
+        const subplan : action[] = randomMember(candidate_subplans);
+        let children : Solution[] = [];
+        for(let i=0; i<subplan.length; i++) {
+            const childSol : Solution = expandHTN(subplan[i]);
+            children.push(childSol);
+        }
+        return {action_type: comp.action_type, children: children};
+    }
+    throw new Error("No decomposition found for action "+action.action_type);
+}
+
+
+/*
+function linearize(s: Solution) : PrimitiveAction[] {
+
+}
+*/
+ 
+
 
 
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,7 +1,13 @@
 import {square} from "./whateveryouwant"
+import {expandHTN, composite_propel_by_explosion, randomMember} from "./htn"
 
 console.log("A")
 const foo = 5;
+const myStuff = [1,2,3,4,5]
+console.log("Random member of myStuff: "+randomMember(myStuff))
+
+console.log("Expanding propel by explosion:")
+console.log(expandHTN(composite_propel_by_explosion));
 
 const mydiv = document.getElementById("main");
 mydiv!.innerHTML = "Hello, world!";
