d703d5f5a822b0d2d5457319ef10edcd304a0c6d
Chris Martens <martens@csc.ncsu.edu>
2019-07-17 17:09:12 -0600

    cleanup

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,12 +1,5 @@
 import * as HTNUnitTest from "./testhtnplanning"
 
-// console.log("A")
-// const foo = 5;
-// const myStuff = [1,2,3,4,5]
-// console.log("Random member of myStuff: "+ HTN.randomMember(myStuff))
-
-// console.log("Expanding propel by explosion:")
-// console.log(expandHTN(composite_propel_by_explosion));
 console.log("---- UNIT TESTS ----")
 console.log("applyTask():");
 HTNUnitTest.testApplyTaskInApplicableState();
